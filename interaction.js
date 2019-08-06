71794194b7c729b8fcfc6428fbb5f6357fc87c16
marcusmASC6 <52749690+marcusmASC6@users.noreply.github.com>
2019-08-06 13:38:58 -0400

    New and Improved
    -Logo(Favicon as well)
    -More Text
    -New photo(TBD if it will be kept
    -Text commands-->button commands
    -Media query still ad issues

--- interaction.js
+++ interaction.js
@@ -5,11 +5,11 @@ function menuclickAbout(){
 }
 document.getElementById('backgroundmenu').addEventListener('click', menuclickbackstory)
 function menuclickbackstory(){
-    (document.getElementById('backstory')).scrollIntoView({behavior:"smooth", block: "start"});
+    (document.getElementById('backstory')).scrollIntoView({behavior:"smooth", block: "start", inline: 'start'});
 }
 document.getElementById('Featuredmenu').addEventListener('click', menuclickFeatured)
 function menuclickFeatured(){
-    document.getElementById('Featured').scrollIntoView({behavior:"smooth", block: "center", inline: "nearest"});
+    document.getElementById('Featured').scrollIntoView({behavior:"smooth", block: "start", inline: "start"});
 }
 document.getElementById('Future-Goalmenu').addEventListener('click', menuclickFuture)
 function menuclickFuture(){
@@ -17,5 +17,14 @@ function menuclickFuture(){
 }
 document.getElementById('Familymenu').addEventListener('click',menuclickFamily)
 function menuclickFamily(){
-    document.getElementById('Family').scrollIntoView({behavior:"smooth", block: "center", inline: "start"});
+    document.getElementById('Family').scrollIntoView({behavior:"smooth", block: "start", inline: "start"});
+}
+document.getElementById('Picture').addEventListener('click',menuclickPicture)
+function menuclickPicture(){
+    document.getElementById('imgAbout').scrollIntoView({behavior:"smooth", block: "start", inline: "start"});
+}
+
+document.getElementById('Resume').addEventListener('click', resumeURL)
+function resumeURL(){
+    window.location.href = "resume.pdf";
 }
